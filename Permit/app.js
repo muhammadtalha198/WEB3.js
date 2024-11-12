d182f705eec7939e2ac537608e9083a5575a6184
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-11-12 09:58:20 +0500

    Update app.js

--- Permit/app.js
+++ Permit/app.js
@@ -1,1 +1,14 @@
+import "./App.css";
+import PermitComponent from "./PermitComponent";
+// import SplitComponent from "./SplitComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <PermitComponent />
+    </div>
+  );
+}
+
+export default App;
 
